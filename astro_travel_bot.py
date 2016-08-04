b9ab8c9092516b120305e1e844ddeef9dfcd270a
Eve Chase <eachase@email.wm.edu>
2016-08-04 15:19:25 -0500

    Outline of initial code to tweet location at new followers

---
+++ astro_travel_bot.py
@@ -0,0 +1,14 @@
+from __future__ import division
+
+
+if __name__ == '__main__':
+	# Check if you were followed
+
+	# Get location of recent follower
+
+	# Tweet something related to the location at this recent follower
+
+	#
+
+
+
