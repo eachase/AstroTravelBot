fac1710819e37fcb78a75ab90c5e704276f4a0d2
WINDYFLY <domi.kingdom@gmail.com>
2016-08-05 09:41:32 -0500

    add weather api and get basic weather based on profile location

--- view_locations.py
+++ view_locations.py
@@ -8,8 +8,22 @@ auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
 
+import pyowm  
+owm = pyowm.OWM('ec4c93234544d284adf07e0e4e89660e')
+
 #import twitter
 #users = api.get_user('mjn693')
 users = api.get_user('astrotravelbot')
-for friend in users.friends():
-    print friend.location
+
+#for friend in users.friends():
+#    print friend.location
+
+
+for friend in users.followers():
+   print(friend.location)
+   try:
+       observation = owm.weather_at_place(friend.location)  
+       w = observation.get_weather()  
+       print(w.get_wind()) # see attribute list for different weather info.
+   except:
+       print("no location found")
